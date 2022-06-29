ecf9aee91ee134cf43647854201a8db630eaceb9
Alvarez Adrien <adrien.alvarez.vanhard@gmail.com>
2022-06-29 17:26:47 +0200

    DAO -> L'ajout d'un article ajoute le Retrait correspondant en BDD

--- src/main/java/fr/eni/tp/enchere/dal/jdbc/ArticleVenduDAOJdbcImpl.java
+++ src/main/java/fr/eni/tp/enchere/dal/jdbc/ArticleVenduDAOJdbcImpl.java
@@ -28,33 +28,52 @@ public class ArticleVenduDAOJdbcImpl {
 		
 		try(Connection cnx = ConnectionProvider.getConnection()){
 			
-			PreparedStatement ordre = cnx.prepareStatement(REQ_INSERT_ARTICLE_VENDU, Statement.RETURN_GENERATED_KEYS);
-			
-			ordre.setString(1, article.getNomArticle());
-			ordre.setString(2, article.getDescription());
-			ordre.setDate(3, Date.valueOf(article.getDateDebutEncheres()));
-			ordre.setDate(4, Date.valueOf(article.getDateFinEncheres()));
-			ordre.setInt(5, article.getPrixVente());
-			ordre.setInt(6, article.getVendeur().getNoUtilisateur());
-			ordre.setInt(7, article.getCategorie().getNoCategorie());
-						
-			ordre.executeUpdate();
-			
-			//Insert le Retrait correspondant à l'article
-			
-//			ResultSet rs = ordre.getGeneratedKeys();
-//			
-//			if (rs.next()) 
-//			{
-//				int newId = rs.getInt(1);
-//				
-//				article.setNoArticle(newId);
-//
-//			}		
-			
+			try {
+				
+				cnx.setAutoCommit(false);
+				
+				PreparedStatement ordre = cnx.prepareStatement(REQ_INSERT_ARTICLE_VENDU, Statement.RETURN_GENERATED_KEYS);
+				
+				ordre.setString(1, article.getNomArticle());
+				ordre.setString(2, article.getDescription());
+				ordre.setDate(3, Date.valueOf(article.getDateDebutEncheres()));
+				ordre.setDate(4, Date.valueOf(article.getDateFinEncheres()));
+				ordre.setInt(5, article.getPrixVente());
+//				ordre.setInt(6, article.getVendeur().getNoUtilisateur());
+				ordre.setInt(6, 2);
+				ordre.setInt(7, article.getCategorie().getNoCategorie());
+							
+				ordre.executeUpdate();
+				
+				ResultSet rs = ordre.getGeneratedKeys();
+				
+				int noArticle = -1;
+				
+				if (rs.next()) 
+				{
+					noArticle = rs.getInt(1);	
+					
+					//Insert le Retrait correspondant à l'article
+					retraitDAO.insertRetrait(cnx, noArticle, article.getRetrait());
+
+				}	
+				
+				cnx.commit();
+				
+			} catch (SQLException e) {
+				
+				cnx.rollback();
+				
+				System.err.println("Erreur lors de la transaction dans insertArticleVendu()");
+				e.printStackTrace();
+				
+			}
+
 		} catch (SQLException e) {
+
 			System.err.println("Erreur lors de l'éxécution de insertArticleVendu()");
 			e.printStackTrace();
+
 		}
 	
 	}// Eo insertArticleVendu()
--- src/main/java/fr/eni/tp/enchere/dal/jdbc/RetraitDAOJdbcIImpl.java
+++ src/main/java/fr/eni/tp/enchere/dal/jdbc/RetraitDAOJdbcIImpl.java
@@ -12,18 +12,18 @@ public class RetraitDAOJdbcIImpl {
 	private final static String REQ_INSERT_RETRAIT 
 	= "INSERT RETRAITS(no_article, rue, code_postal, ville) VALUES (?,?,?,?)";
 	
-	public void insertRetrait(int numeroArticle, Retrait retrait) {
+	public void insertRetrait(Connection cnx, int numeroArticle, Retrait retrait) {
 		
-		try(Connection cnx = ConnectionProvider.getConnection()){
+		try {
 			
-			PreparedStatement ordre = cnx.prepareStatement(REQ_INSERT_RETRAIT, Statement.RETURN_GENERATED_KEYS);
+			PreparedStatement req = cnx.prepareStatement(REQ_INSERT_RETRAIT, Statement.RETURN_GENERATED_KEYS);
 			
-			ordre.setInt(1, numeroArticle);
-			ordre.setString(2, retrait.getRue());
-			ordre.setString(3, retrait.getCode_postal());
-			ordre.setString(4, retrait.getVille());
+			req.setInt(1, numeroArticle);
+			req.setString(2, retrait.getRue());
+			req.setString(3, retrait.getCode_postal());
+			req.setString(4, retrait.getVille());
 						
-			ordre.executeUpdate();	
+			req.executeUpdate();	
 			
 		} catch (SQLException e) {
 			System.err.println("Erreur lors de l'éxécution de insertRetrait()");
